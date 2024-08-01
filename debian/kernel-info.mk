f7e94d6543d6b1b0545499174e77e3f1342a9e59
r0ddty <r0d@ukr.net>
2024-08-01 17:21:07 +0300

    Changed DTS path
    Signed-off-by: r0ddty <r0d@ukr.net>

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -49,7 +49,7 @@ KERNEL_IMAGE_WITH_DTB = 1
 # Path to the DTB
 # If you leave this undefined, an attempt to find it automatically
 # will be made.
-KERNEL_IMAGE_DTB = arch/arm/boot/dts/mt6765.dtb
+KERNEL_IMAGE_DTB = arch/arm64/boot/dts/mediatek/cactus.dtb
 
 # Whether to include a DTB Overlay. Use 0 (no) or 1.
 # GKI devices should set this to 0
